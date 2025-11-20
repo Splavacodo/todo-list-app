159b59bbc5083cd93b4e158d4fd27d583beaf1e3
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2025-11-20 14:14:12 -0700

    Updated folder structure to be more organized and made modificiations to config files to match new structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.ts",
+  entry: "./src/ts/app.ts",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
