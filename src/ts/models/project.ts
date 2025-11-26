dea9aa1d4ddf01e946779372b195a0a67efbbeaa
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2025-11-25 23:42:54 -0700

    Created a models directory to store classes which store data for different project components and added an initial implementation for a Project class

---
+++ src/ts/models/project.ts
@@ -0,0 +1,22 @@
+import { Section } from "./section";
+import { Task } from "./task";
+
+export class Project {
+    private _id: string;
+    private _name: string;
+    private _children: Array<Section|Task>;
+
+    constructor(id: string, name: string, children: Array<Section|Task>) {
+        this._id = id;
+        this._name = name;
+        this._children = children;
+    }
+
+    get id(): string { return this._id; }
+
+    get name(): string { return this._name; }
+
+    set name(newName: string) { this._name = newName; }
+
+    get children(): Array<Section|Task> { return this._children };
+}
