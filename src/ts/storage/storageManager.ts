60c2a86a4a423ec5b1c8953e6782faad41c4e54c
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2025-11-25 23:38:36 -0700

    Created storage directory to store ts files related to storage management and added an initial outline for a StorageManger class

---
+++ src/ts/storage/storageManager.ts
@@ -0,0 +1,13 @@
+export class StorageManager {
+    static getLocalProjects() {
+        console.log("TODO: Implement logic to get projects JSON from local storage"); 
+    }
+
+    static getLocalSections() {
+        console.log("TODO: Implement logic to get sections JSON from local storage"); 
+    }
+
+    static getLocalTasks() {
+        console.log("TODO: Implement logic to get tasks JSON from local storage"); 
+    }
+}
