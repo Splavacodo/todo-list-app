d57e956e38d608233afdd1431cb5ed2ce3efed93
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2025-11-27 15:29:32 -0700

    Updated addSectionFromJson method and added getTaskModels method to match the planned format for project, section, and task storage

--- src/ts/services/SectionService.ts
+++ src/ts/services/SectionService.ts
@@ -1,19 +1,40 @@
 import { Section } from "../models/Section";
+import { Task } from "../models/Task";
+
 import { StorageManager } from "../storage/StorageManager";
+
+import { TaskService } from "../services/TaskService";
 
 export class SectionService {
     private sections: Record<string, Section>;
     private storageManager: StorageManager;
+    private taskService: TaskService;
 
-    constructor(storageManager: StorageManager) {
+    constructor(taskService: TaskService, storageManager: StorageManager) {
         this.sections = {};
         this.storageManager = storageManager;
+        this.taskService = taskService;
     }
 
     addSectionFromJson(sectionJson: string): void {
-        let section: Section = JSON.parse(sectionJson);
+        const section: Record<string, any> = JSON.parse(sectionJson);
 
-        this.sections[section.id] = section;
+        const sectionId: string = section["id"];
+        const sectionName: string = section["name"];
+        const sectionTaskIds: Array<string> = section["taskIds"];
+        const parentId: string = section["parentId"];
+        const sectionTasks: Array<Task> = this.getTaskModels(sectionTaskIds);
+
+        this.sections[sectionId] = new Section(sectionId, sectionName, sectionTasks, parentId);
+    }
+
+    private getTaskModels(sectionTaskIds: Array<string>): Array<Task> {
+        const tasks: Array<Task> = [];
+
+        for(let taskId of sectionTaskIds)
+            tasks.push(this.taskService.getTask(taskId));
+
+        return tasks;
     }
 
     getSection(sectionId: string): Section { return this.sections[sectionId]; }
