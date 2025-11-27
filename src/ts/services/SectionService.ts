7a26dc42ff9a569bd13808c6b1b33a4206962304
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2025-11-27 13:14:05 -0700

    Removed underscore prefix on private variables since they do not add any protection and renamed methods to include section rather than task

--- src/ts/services/SectionService.ts
+++ src/ts/services/SectionService.ts
@@ -2,23 +2,23 @@ import { Section } from "../models/Section";
 import { StorageManager } from "../storage/StorageManager";
 
 export class SectionService {
-    private _sections: Record<string, Section>;
-    private _storageManager: StorageManager;
+    private sections: Record<string, Section>;
+    private storageManager: StorageManager;
 
     constructor(storageManager: StorageManager) {
-        this._sections = {};
-        this._storageManager = StorageManager;
+        this.sections = {};
+        this.storageManager = storageManager;
     }
 
     addSectionFromJson(sectionJson: string): void {
         let section: Section = JSON.parse(sectionJson);
 
-        this._sections[section.id] = section;
+        this.sections[section.id] = section;
     }
 
-    getTask(sectionId: string): Section { return this._sections[sectionId]; }
+    getSection(sectionId: string): Section { return this.sections[sectionId]; }
 
-    deleteTask(sectionId: string): void { delete this._sections[sectionId]; }
+    deleteSection(sectionId: string): void { delete this.sections[sectionId]; }
 
     // Will be extending this class for more functionality later
 }
