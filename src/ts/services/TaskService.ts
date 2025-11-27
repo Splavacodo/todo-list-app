9be2a95a084ba45d5e345a64779fd0b1d956768b
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2025-11-27 13:12:06 -0700

    Removed underscore prefix for private variables since it does not add to protection and added a method to help with identifying project children in TaskService

--- src/ts/services/TaskService.ts
+++ src/ts/services/TaskService.ts
@@ -2,23 +2,25 @@ import { Task } from "../models/Task";
 import { StorageManager } from "../storage/StorageManager";
 
 export class TaskService {
-    private _tasks: Record<string, Task>;
-    private _storageManager: StorageManager;
+    private tasks: Record<string, Task>;
+    private storageManager: StorageManager;
 
     constructor(storageManager: StorageManager) {
-        this._tasks = {};
-        this._storageManager = StorageManager;
+        this.tasks = {};
+        this.storageManager = storageManager;
     }
 
     addTaskFromJson(taskJson: string): void {
         let task: Task = JSON.parse(taskJson);
 
-        this._tasks[task.id] = task;
+        this.tasks[task.id] = task;
     }
 
-    getTask(taskId: string): Task { return this._tasks[taskId]; }
+    containsId(id: string): boolean { return id in this.tasks; }
 
-    deleteTask(taskId: string): void { delete this._tasks[taskId]; }
+    getTask(taskId: string): Task { return this.tasks[taskId]; }
+
+    deleteTask(taskId: string): void { delete this.tasks[taskId]; }
 
     // Will be extending this class for more functionality later
 }
